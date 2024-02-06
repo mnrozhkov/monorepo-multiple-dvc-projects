3307247f882514ef838078dc1f104b0b1a7d5a91
Mikhail <mnrozhkov@gmail.com>
2024-02-06 09:29:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Tests for Monorepo scenarios in DVC Studio
+
+## Install 
+
+1. Fork the repo
+2. Create project in Studio 
+3. Clone & install deps
+
+```bash
+git clone https://github.com/mnrozhkov/monorepo-multiple-dvc-projects.git
+cd monorepo-multiple-dvc-projects
+
+python -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt 
+```
+
+## Run tests 
+
+```bash
+chmod +x scripts/* 
+
+# Example for test 5_1
+./scripts/5_1_test_dvc_exp_push_inProjectA_parentCommit_projectA.sh
+```
---
+++ scripts/3_test_dvc_exp_inProjectA_parentCommit_inProjectB.sh
@@ -0,0 +1,15 @@
+# (step 1) Prepare the starting commit - in project_b 
+echo "$(date '+%Y-%m-%d %H:%M:%S') - 3-test_dvc_exp_inProjectA_parentCommit_inProjectB.sh" >> project_b/some_projectB_content
+git add project_b/some_projectB_content
+git commit -m "3-test_dvc_exp_inProjectA_parentCommit_inProjectB.sh"
+git push origin main # -> only changes in project_b/some_projectB_content
+
+# (step 2) Run expriment in A
+cd project_a
+dvc exp run -f
+
+# (step 3) Push DVC experiment from Project A
+cd ..
+git add project_a/*
+git commit -m "Commit dvc exp: 3-test_dvc_exp_inProjectA_parentCommit_inProjectB.sh"
+git push origin main 
